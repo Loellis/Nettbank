c7a851ab8bdcda6e48d554d5f6bab05b3dea6966
Loellis <loellis92@gmail.com>
2016-10-18 11:31:47 +0200

    Lagt til flat konto

---
+++ Nettbank/Models/Konto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nettbank.Models
+{
+    public class Konto
+    {
+        public int kontoId { get; set; }
+        public int personnummer { get; set; }
+        public int saldo { get; set; }
+    }
+}
