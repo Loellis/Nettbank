01e387d6d76a911d0ffdca82ee7ba58a6133b058
Loellis <loellis92@gmail.com>
2016-10-18 10:13:23 +0200

    Endret Codefirst kunde til DBContext og flat Kunde.cs

--- Nettbank/Models/Kunde.cs
+++ Nettbank/Models/Kunde.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -10,36 +7,12 @@ namespace Nettbank.Models
 {
     public class Kunde
     {
-        [Key]
-        public int id { get; set; } //Kundenummer
-        public int Personnummer { get; set; }
-        public string Fornavn { get; set; }
-        public string Etternavn { get; set; }
-        public string Adresse { get; set; }
-        public virtual PostSted Poststed { get; set; }
-    }
-
-    public class PostSted
-    {
-        [Key]
-        public string Postnr { get; set; }
-        public string Poststed { get; set; }
-    }
-
-    public class KundeContext : DbContext
-    {
-        public KundeContext()
-            : base("name=Kunde")
-        {
-            Database.CreateIfNotExists();
-        }
-
-        public DbSet<Kunde> Kunder { get; set; }
-        public DbSet<PostSted> Poststeder { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }
+        public int id { get; set; }
+        public int personnummer { get; set; }
+        public string fornavn { get; set; }
+        public string etternavn { get; set; }
+        public string adresse { get; set; }
+        public string postnr { get; set; }
+        public string poststed { get; set; }
     }
 }
