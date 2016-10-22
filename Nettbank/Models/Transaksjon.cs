0d76bc0b64c9f8c284cc5004f15a29e1f281d896
Loellis <loellis92@gmail.com>
2016-10-22 13:18:26 +0200

    -Dataannotasjoner for Transaksjon.cs

--- Nettbank/Models/Transaksjon.cs
+++ Nettbank/Models/Transaksjon.cs
@@ -9,13 +9,30 @@ namespace Nettbank.Models
     public class Transaksjon
     {
         [Key]
+        [Display(Name ="Transaksjons ID")]
         public int TransaksjonsID { get; set; }
-        public int Utkonto { get; set; }
-        public int Innkonto { get; set; }
-        public double Beløp { get; set; }
-        public long KID { get; set; }
+
+        [Display(Name ="Fra Konto")]
+        [Required(ErrorMessage = "Velg konto du ønsker å overføre beløpet fra")]
+        public string Utkonto { get; set; }
+
+        [Display(Name ="Til Konto")]
+        [Required(ErrorMessage = "Velg konto du ønsker å overføre beløpet til")]
+        public string Innkonto { get; set; }
+
+        [Display(Name = "Beløp")]
+        [Required(ErrorMessage ="Velg beløpet som skal overføres")]
+        public string Beløp { get; set; }
+
+        [Display(Name ="KID")]
+        public string KID { get; set; }
+
+        [Display(Name ="Melding")]
         public string Melding { get; set; }
+
+        [Display(Name ="Time of Transaction")]
         public string Tidspunkt { get; set; }
-        public int TilhørendeKonto { get; set; }
+
+        public string TilhørendeKonto { get; set; }
     }
 }
